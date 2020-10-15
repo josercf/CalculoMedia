5150cdd3e9953dfe8c3f25637fb695c06c53d07e
José Romualdo <jose.romualdo@unidas.com.br>
2020-10-14 21:11:26 -0300

    Add project files.

---
+++ CalculoDeNota/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculoDeNota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Avaliacao av1 = new Avaliacao();
+            Avaliacao av2 = new Avaliacao();
+            Avaliacao av3 = new Avaliacao();
+
+            av1.NotaTrabalho = 4.5F;
+            av1.NotaProva = 6;
+
+            av2.NotaTrabalho = 7.3F;
+            av2.NotaProva = 3;
+
+            av3.NotaProva = 6.6F;
+
+            List<Avaliacao> notas = new List<Avaliacao>();
+            notas.Add(av1);
+            notas.Add(av2);
+            notas.Add(av3);
+
+            float media = notas.OrderByDescending(x => x.Nota)
+                               .Take(2)
+                               .Sum(c => c.Nota) / 2;
+
+
+            var soma = 0F;
+            for (int i = 0; i < notas.Count; i++)
+            {
+                soma += notas[i].Nota;
+            }
+
+
+
+
+
+
+
+
+
+
+            //3 - Avaliações
+            //AV1 - 40% de trabalhos e 60% nota da Prova;
+            //AV2 - 40% de trabalhos e 60% nota da Prova;
+            //AV3 - 100% nota da Prova;
+
+            //AV1   AV2   AV3   Média     Resultado
+            // 7      4     3     5,5       Reprovado
+
+
+
+
+            //Descarta a menor nota e tira-se a média das duas restantes;
+            //Par o aluno ser aprovado, a média precisa ser igual ou maior a 6;
+
+            //5 disciplinas;
+            //7 alunos;
+        }
+    }
+
+
+    class Avaliacao
+    {
+        public float NotaTrabalho { get; set; }
+        public float NotaProva { get; set; }
+
+        public float Nota => (NotaTrabalho * 0.4F) + (NotaProva * 0.6F);
+
+        //int: 5
+        //float:   5.51234                        4 bytes
+        //double:  5.12345678901234               8 bytes  
+        //decimal: 0.12345678901234567890123456  16 bytes
+
+        //         0,0052054794520548
+
+        //1000 *
+
+
+
+        //0,000012
+    }
+}
---
+++ DemoIntroducao/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoIntroducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Aluno> alunos = new List<Aluno>();
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                Aluno aluno = new Aluno();
+
+
+                Console.WriteLine("Informe o nome do aluno:");
+                aluno.Nome = Console.ReadLine();
+
+                if(aluno.NomeEhValido() == false)
+                {
+                    Console.WriteLine("O nome digitado é inválido");
+                }
+
+
+                Console.WriteLine("Informe o endereço do aluno:");
+                aluno.Endereco = Console.ReadLine();
+
+                Console.WriteLine("Informe a data de nascimento do aluno:");
+                aluno.DataNascimento = Console.ReadLine();
+
+
+                alunos.Add(aluno);
+
+
+                Console.WriteLine("Deseja cadastrar um novo aluno? S - Sim/N - Não");
+                string resposta = Console.ReadLine();
+
+                if (resposta == "N")
+                    break;
+
+
+            }
+
+            Console.WriteLine("******************");
+
+
+            Console.WriteLine("Nome        | Endereço         | Dt. Nasc.       ");
+            foreach (var item in alunos)
+            {
+                Console.WriteLine($"{item.Nome} | {item.Endereco} | {item.DataNascimento}");
+            }
+
+
+            //Console.WriteLine($"Nome: { aluno.Nome}");
+            //Console.WriteLine($"Endereço: { aluno.Endereco}");
+            //Console.WriteLine($"Data Nasc.: { aluno.DataNascimento}");
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome;
+        public string DataNascimento;
+        public string Endereco;
+        public string RA;
+
+
+        public bool NomeEhValido()
+        {
+            return Nome.Length > 5;
+        }
+    }
+}
